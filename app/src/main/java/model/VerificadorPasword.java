6fa20611fa128f415d0aebae4903b41c569c3b44
Rafael <rafael.calderonp@gmail.com>
2021-01-17 10:35:27 -0300

    Comit, cambio en presentador y en model, Se suma una nueva condición

--- app/src/main/java/model/VerificadorPasword.java
+++ app/src/main/java/model/VerificadorPasword.java
@@ -3,33 +3,61 @@ package model;
 public class VerificadorPasword {
 
     Dev dev = new Dev();
+    int mayusc;
 
-    public boolean VerificadorExtens (String password){
+   /* public boolean VerificadorExtens (String password){
         return password.length() < 5;
+    }*/
 
-    }
-
-    public boolean VerificadorMayusc (String password){
+   /* public boolean VerificadorMayusc (String password){
         return  password.toLowerCase().equals(password);
+    }*/
+
+    public int LargoPassword (String password){
+        return password.length();
     }
+
+    public int ContadorMayusc (String password){
+        mayusc = 0;
+        int length = password.length();
+        String passwordM = password.toUpperCase();
+        String passwordm = password.toLowerCase();
+
+        for (int i = 0; i < length; i++ ){
+            if(!(password.charAt(i) == passwordm.charAt(i))
+                    && (password.charAt(i) == passwordM.charAt(i))){
+                mayusc = 1 + mayusc;
+            }
+        }
+        return mayusc;
+    }
+
 
     public Dev CheckerStrength(String password){
-        if (VerificadorExtens(password)){
+        if (LargoPassword(password)<3){
             dev.colour = "#D50D0D";
             dev.condition = "Weak Password";
             return (dev);
         }
         else {
-            if (VerificadorMayusc(password)){
-                dev.colour = "#FFEB3B";
-                dev.condition = "Medium Password";
+            if ((LargoPassword(password)>=3 && LargoPassword(password)<5)){
+                dev.colour = "#FF9800";
+                dev.condition = "Normal Password";
                 return (dev);
             }
-            else{
-                dev.colour = "#76FF3B";
-                dev.condition = "Strong Password";
-                return (dev);
-            }
+            else {
+                if (ContadorMayusc(password)<2){
+                    dev.colour = "#FFEB3B";
+                    dev.condition = "Medium Password";
+                    return (dev);
+                }
+                else{
+                    dev.colour = "#76FF3B";
+                    dev.condition = "Strong Password";
+                    return (dev);
+                }
+        }
+
         }
     }
 public class Dev {
@@ -40,17 +68,17 @@ public class Dev {
         return colour;
     }
 
-    public void setColour(String colour) {
+   /* public void setColour(String colour) {
         this.colour = colour;
-    }
+    }*/
 
     public String getCondition() {
         return condition;
     }
 
-    public void setCondition(String condition) {
+   /* public void setCondition(String condition) {
         this.condition = condition;
-    }
+    }*/
 }
 
 }
